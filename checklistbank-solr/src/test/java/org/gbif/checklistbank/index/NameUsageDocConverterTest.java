aa90eaeffa38b3f448a6fb9124b744d6dcf33a29
Markus Döring <m.doering@mac.com>
2014-10-20 12:54:35 +0200

    Index name usage issues in solr

--- checklistbank-solr/src/test/java/org/gbif/checklistbank/index/NameUsageDocConverterTest.java
+++ checklistbank-solr/src/test/java/org/gbif/checklistbank/index/NameUsageDocConverterTest.java
@@ -1,14 +1,17 @@
 package org.gbif.checklistbank.index;
 
 import org.gbif.api.model.checklistbank.NameUsage;
+import org.gbif.api.vocabulary.NameUsageIssue;
 import org.gbif.api.vocabulary.Rank;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.apache.solr.common.SolrInputDocument;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.*;
 
 public class NameUsageDocConverterTest {
 
@@ -23,6 +26,8 @@ public class NameUsageDocConverterTest {
     u.setRank(Rank.SPECIES);
     u.setSynonym(false);
     u.setParentKey(1);
+    u.getIssues().add(NameUsageIssue.RANK_INVALID);
+    u.getIssues().add(NameUsageIssue.BACKBONE_MATCH_FUZZY);
 
     SolrInputDocument doc = conv.toObject(u, null, null, null, null);
     assertEquals(u.getKey().toString(), doc.get("key").getValue());
@@ -31,5 +36,7 @@ public class NameUsageDocConverterTest {
     assertEquals(u.getCanonicalName(), doc.get("canonical_name").getValue());
     assertEquals(u.getScientificName(), doc.get("scientific_name").getValue());
     assertEquals(u.getRank().ordinal(), doc.get("rank_key").getValue());
+    assertThat((List<?>)doc.get("issues").getValue()).contains(NameUsageIssue.RANK_INVALID.ordinal(),
+      NameUsageIssue.BACKBONE_MATCH_FUZZY.ordinal());
   }
 }
