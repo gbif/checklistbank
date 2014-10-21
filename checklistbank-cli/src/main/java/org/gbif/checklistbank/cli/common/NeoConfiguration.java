19f2e4ab71a6f179e73cb515f2ed66d68f9d4fd3
Markus Döring <m.doering@mac.com>
2014-10-21 12:37:06 +0200

    Upgrade to neo 2.1.5 and use new simplified memory settings

--- checklistbank-cli/src/main/java/org/gbif/checklistbank/cli/common/NeoConfiguration.java
+++ checklistbank-cli/src/main/java/org/gbif/checklistbank/cli/common/NeoConfiguration.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotNull;
 import com.beust.jcommander.Parameter;
 import com.google.common.base.Preconditions;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseBuilder;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.slf4j.Logger;
@@ -29,30 +30,24 @@ public class NeoConfiguration {
   @Parameter(names = "--neo-batchsize")
   public int batchSize = 10000;
 
-  @Min(0)
-  @Parameter(names = "--neo-memory")
-  public int memory = 0;
+  @Parameter(names = "--neo-debug")
+  public boolean debug = false;
 
-  @Parameter(names = "--neo-nodestore-memory")
-  public int nodestoreMemory= 128;
+  /**
+   * none, soft, weak, strong
+   * see http://docs.neo4j.org/chunked/stable/configuration-caches.html
+   */
+  @NotNull
+  @Parameter(names = "--neo-cache-type")
+  public String cacheType = "soft";
 
-  @Parameter(names = "--neo-propertystore-memory")
-  public int propertystoreMemory = 64;
+  @Min(16)
+  @Parameter(names = "--neo-mapped-memory")
+  public int mappedMemory = 64;
 
-  @Parameter(names = "--neo-propertystore-index-keys-memory")
-  public int propertystoreIndexKeysMemory = 64;
-
-  @Parameter(names = "--neo-relationshipstore-memory")
-  public int relationshipstoreMemory = 256;
-
-  @Parameter(names = "--neo-memory-page")
-  public int memoryPage = 64;
-
-  @Parameter(names = "--neo-strings-memory")
-  public int stringsMemory = 32;
-
-  @Parameter(names = "--neo-arrays-memory")
-  public int arraysMemory = 32;
+  @Min(64)
+  @Parameter(names = "--neo-mapped-memory-page-size")
+  public int mappedMemoryPageSize = 8192;
 
   public File neoDir(UUID datasetKey) {
     return new File(neoRepository, datasetKey.toString());
@@ -66,40 +61,24 @@ public class NeoConfiguration {
   public GraphDatabaseService newEmbeddedDb(UUID datasetKey) {
     Preconditions.checkNotNull(datasetKey);
     File storeDir = neoDir(datasetKey);
-    applyMemoryConfig();
     GraphDatabaseFactory factory = new GraphDatabaseFactory();
-    GraphDatabaseService db = factory.newEmbeddedDatabaseBuilder(storeDir.getAbsolutePath())
-      .setConfig(GraphDatabaseSettings.cache_type, "none")
-      .setConfig(GraphDatabaseSettings.nodestore_mapped_memory_size, mb(nodestoreMemory))
-      .setConfig(GraphDatabaseSettings.nodestore_propertystore_index_keys_mapped_memory_size, mb(propertystoreIndexKeysMemory))
-      .setConfig(GraphDatabaseSettings.nodestore_propertystore_mapped_memory_size, mb(propertystoreMemory))
-      .setConfig(GraphDatabaseSettings.relationshipstore_mapped_memory_size, mb(relationshipstoreMemory))
-      .setConfig(GraphDatabaseSettings.mapped_memory_page_size, mb(memoryPage))
-      .setConfig(GraphDatabaseSettings.strings_mapped_memory_size, mb(stringsMemory))
-      .setConfig(GraphDatabaseSettings.arrays_mapped_memory_size, mb(arraysMemory))
-      .newGraphDatabase();
+    GraphDatabaseBuilder builder = factory.newEmbeddedDatabaseBuilder(storeDir.getAbsolutePath())
+      .setConfig(GraphDatabaseSettings.keep_logical_logs, "false")
+      .setConfig(GraphDatabaseSettings.cache_type, cacheType)
+      .setConfig(GraphDatabaseSettings.mapped_memory_page_size, mb(mappedMemoryPageSize))
+      .setConfig(GraphDatabaseSettings.all_stores_total_mapped_memory_size, mb(mappedMemory));
+    if (debug) {
+      builder
+        .setConfig(GraphDatabaseSettings.log_mapped_memory_stats, "true")
+        .setConfig(GraphDatabaseSettings.dump_configuration, "true");
+    }
+    GraphDatabaseService db = builder.newGraphDatabase();
     LOG.info("Starting embedded neo4j database from {}", storeDir.getAbsolutePath());
     return db;
   }
 
   private String mb(int memoryInMB) {
     return memoryInMB + "M";
-  }
-
-  /**
-   * If the single total memory setting is configured use it to override the individual neo memory configurations.
-   */
-  private void applyMemoryConfig(){
-    if (memory > 0) {
-      final int sixteenth = memory / 16;
-      nodestoreMemory = sixteenth * 3;
-      relationshipstoreMemory = sixteenth*8;
-      propertystoreMemory = sixteenth;
-      propertystoreIndexKeysMemory = sixteenth;
-      memoryPage = sixteenth;
-      stringsMemory = sixteenth;
-      arraysMemory = sixteenth;
-    }
   }
 
 }
