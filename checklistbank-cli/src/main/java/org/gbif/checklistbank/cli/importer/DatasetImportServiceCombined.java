d2c7778d432b957a81663da74def2bc66c2a3b7a
Markus Döring <m.doering@mac.com>
2015-08-06 13:02:33 +0200

    Use a single threaded executor for solr syncs during import

--- checklistbank-cli/src/main/java/org/gbif/checklistbank/cli/importer/DatasetImportServiceCombined.java
+++ checklistbank-cli/src/main/java/org/gbif/checklistbank/cli/importer/DatasetImportServiceCombined.java
@@ -10,6 +10,8 @@ import org.gbif.checklistbank.service.DatasetImportService;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import javax.annotation.Nullable;
 
 import com.google.common.base.Preconditions;
@@ -17,15 +19,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * A thin wrapper that delegates all import methods to both postgres and solr.
- * Solr index updates are handled asynchroneously in a separate thread but if the queue gets too large
- * it will block the current thread until the queue is small enough again.
+ * A thin wrapper that delegates all import methods to both postgres and solr using independent threads to parallelize the updates to both.
  */
 public class DatasetImportServiceCombined {
     private static final Logger LOG = LoggerFactory.getLogger(DatasetImportServiceCombined.class);
 
-    private final DatasetImportService sqlService;
     private final NameUsageIndexService solrService;
+    ExecutorService solrExecutor = Executors.newSingleThreadExecutor();
+    private final DatasetImportService sqlService;
 
     public DatasetImportServiceCombined(DatasetImportService sqlService, NameUsageIndexService solrService) {
         this.sqlService = sqlService;
@@ -35,13 +36,13 @@ public class DatasetImportServiceCombined {
     public int syncUsage(boolean insert, NameUsage usage, List<Integer> parents, @Nullable VerbatimNameUsage verbatim, NameUsageMetrics metrics, @Nullable UsageExtensions extensions) {
         Preconditions.checkNotNull(usage.getDatasetKey(), "datasetKey must exist");
         int key = sqlService.syncUsage(insert, usage, verbatim, metrics, extensions);
-        solrService.insertOrUpdate(usage, parents, extensions);
+        solrExecutor.submit(new SolrUpdate(usage, parents, extensions));
         return key;
     }
 
     public void updateForeignKeys(int usageKey, Integer parentKey, Integer basionymKey) {
         sqlService.updateForeignKeys(usageKey, parentKey, basionymKey);
-        solrService.insertOrUpdate(usageKey);
+        solrExecutor.submit(new SolrUpdateById(usageKey));
     }
 
     public int deleteOldUsages(UUID datasetKey, Date before) {
@@ -49,12 +50,54 @@ public class DatasetImportServiceCombined {
         // iterate over all ids to be deleted and remove them from solr first
         int counter = 0;
         for (Integer id : sqlService.listOldUsages(datasetKey, before)) {
-            solrService.delete(id);
             sqlService.delete(id);
+            solrExecutor.submit(new SolrDelete(id));
             counter++;
         }
         LOG.info("Deleted {} usages from dataset {} before {}", counter, datasetKey, before);
         return counter;
     }
 
+    class SolrUpdate implements Runnable {
+        private final NameUsage usage;
+        private final List<Integer> parents;
+        private final UsageExtensions extensions;
+
+        public SolrUpdate(NameUsage usage, List<Integer> parents, UsageExtensions extensions) {
+            this.usage = usage;
+            this.parents = parents;
+            this.extensions = extensions;
+        }
+
+        @Override
+        public void run() {
+            solrService.insertOrUpdate(usage, parents, extensions);
+        }
+    }
+
+    class SolrUpdateById implements Runnable {
+        private final int id;
+
+        public SolrUpdateById(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            solrService.insertOrUpdate(id);
+        }
+    }
+
+    class SolrDelete implements Runnable {
+        private final int id;
+
+        public SolrDelete(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            solrService.delete(id);
+        }
+    }
 }
