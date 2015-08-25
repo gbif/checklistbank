acaf12d155628e00ebf882c27dacf8bdc5277e82
Markus Döring <m.doering@mac.com>
2015-08-25 15:02:42 +0200

    Forgot adding new nub source 21

--- checklistbank-cli/src/test/java/org/gbif/checklistbank/nub/source/ClasspathUsageSourceTest.java
+++ checklistbank-cli/src/test/java/org/gbif/checklistbank/nub/source/ClasspathUsageSourceTest.java
@@ -20,7 +20,7 @@ public class ClasspathUsageSourceTest {
   public void testListSources() throws Exception {
     ClasspathUsageSource src = ClasspathUsageSource.allSources();
     List<NubSource> sources = src.listSources();
-    assertEquals(20, sources.size());
+    assertEquals(21, sources.size());
     assertEquals(1, sources.get(0).priority);
     assertEquals(Rank.FAMILY, sources.get(0).ignoreRanksAbove);
   }
