2da0eda1191db3438b2d1da5ede16ecfa3a5c403
Federico Mendez <fmendez@gbif.org>
2015-11-06 10:36:03 +0100

    changing module to a private service module

--- checklistbank-solr/src/main/java/org/gbif/checklistbank/index/guice/AvroIndexingModule.java
+++ checklistbank-solr/src/main/java/org/gbif/checklistbank/index/guice/AvroIndexingModule.java
@@ -1,35 +1,34 @@
 package org.gbif.checklistbank.index.guice;
 
 import org.gbif.checklistbank.index.NameUsageAvroExporter;
+import org.gbif.checklistbank.index.NameUsageIndexingConfig;
 import org.gbif.checklistbank.service.mybatis.guice.ChecklistBankServiceMyBatisModule;
 import org.gbif.registry.ws.client.guice.RegistryWsClientModule;
+import org.gbif.service.guice.PrivateServiceModule;
 import org.gbif.ws.client.guice.AnonymousAuthModule;
 
 import java.util.Properties;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Scope;
-import com.google.inject.Scopes;
-
 /**
  * Guice module that initializes the required classes and dependencies for the CLB indexer.
  */
-public class AvroIndexingModule extends AbstractModule {
+public class AvroIndexingModule extends PrivateServiceModule {
 
-  private final Properties properties;
 
   public AvroIndexingModule(Properties properties) {
-    this.properties = properties;
+    super(NameUsageIndexingConfig.KEYS_INDEXING_CONF_PREFIX, properties);
   }
 
   @Override
-  protected void configure() {
-    // Installs the MyBatis service layer
-    install(new ChecklistBankServiceMyBatisModule(properties));
-
-    // install registry client
+  protected void configureService() {
+    //installs the MyBatis service layer
+    install(new ChecklistBankServiceMyBatisModule(getVerbatimProperties()));
+    //installs auth module
     install(new AnonymousAuthModule());
-    install(new RegistryWsClientModule(properties));
+    //installs registry client
+    install(new RegistryWsClientModule(getVerbatimProperties()));
 
+    expose(NameUsageAvroExporter.class);
   }
+
 }
