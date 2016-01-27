142778de69c86d096866bee92b9ffded6c5ae4dd
Markus Döring <m.doering@mac.com>
2016-01-27 23:31:49 +0100

    implement datasetmapper update & delete

--- checklistbank-mybatis-service/src/test/java/org/gbif/checklistbank/service/mybatis/mapper/DatasetMapperTest.java
+++ checklistbank-mybatis-service/src/test/java/org/gbif/checklistbank/service/mybatis/mapper/DatasetMapperTest.java
@@ -9,20 +9,29 @@ import static org.junit.Assert.assertNull;
 
 public class DatasetMapperTest extends MapperITBase<DatasetMapper> {
 
-    public DatasetMapperTest() {
-        super(DatasetMapper.class, false);
-    }
+  public DatasetMapperTest() {
+    super(DatasetMapper.class, false);
+  }
 
-    @Test
-    public void truncateInsertGet() {
-        final String title = "Quadrophenia";
+  @Test
+  public void truncateInsertUpdateGet() {
+    final String title = "Quadrophenia";
+    final String title2 = "Quadrophenia reloaded";
 
-        final UUID key = UUID.randomUUID();
-        mapper.truncate();
-        mapper.insert(key, title);
-        assertEquals(title, mapper.get(key));
+    final UUID key = UUID.randomUUID();
+    mapper.truncate();
+    mapper.insert(key, title);
+    assertEquals(title, mapper.get(key));
 
-        mapper.truncate();
-        assertNull(mapper.get(key));
-    }
+    mapper.update(key, title2);
+    assertEquals(title2, mapper.get(key));
+
+    mapper.truncate();
+    assertNull(mapper.get(key));
+
+    mapper.insert(key, title);
+    assertEquals(title, mapper.get(key));
+    mapper.delete(key);
+    assertNull(mapper.get(key));
+  }
 }
