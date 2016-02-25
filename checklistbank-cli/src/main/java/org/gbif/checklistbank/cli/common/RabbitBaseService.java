4022a307bc6700f317169dd7a6ab619150261d5e
Markus Döring <m.doering@mac.com>
2016-02-25 11:59:42 +0100

    Provide new startUpBeforeListening hook to execute setup code before the listener actually responds to messages

--- checklistbank-cli/src/main/java/org/gbif/checklistbank/cli/common/RabbitBaseService.java
+++ checklistbank-cli/src/main/java/org/gbif/checklistbank/cli/common/RabbitBaseService.java
@@ -20,71 +20,79 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class RabbitBaseService<T extends Message> extends AbstractIdleService implements MessageCallback<T> {
-    private static final Logger LOG = LoggerFactory.getLogger(RabbitBaseService.class);
+  private static final Logger LOG = LoggerFactory.getLogger(RabbitBaseService.class);
 
-    private final MessagingConfiguration mCfg;
-    private final GangliaConfiguration gCfg;
-    private final int poolSize;
-    private final String queue;
-    protected final MetricRegistry registry;
-    protected HikariDataSource hds;
-    protected MessagePublisher publisher;
-    protected MessageListener listener;
+  private final MessagingConfiguration mCfg;
+  private final GangliaConfiguration gCfg;
+  private final int poolSize;
+  private final String queue;
+  protected final MetricRegistry registry;
+  protected HikariDataSource hds;
+  protected MessagePublisher publisher;
+  protected MessageListener listener;
 
-    public RabbitBaseService(String queue, int poolSize, MessagingConfiguration mCfg, GangliaConfiguration gCfg) {
-        this.mCfg = mCfg;
-        this.gCfg = gCfg;
-        this.poolSize = poolSize;
-        this.queue = queue;
-        registry = new MetricRegistry(queue);
-        registry.registerAll(new MemoryUsageGaugeSet());
-        registry.register(Metrics.OPEN_FILES, new FileDescriptorRatioGauge());
+  public RabbitBaseService(String queue, int poolSize, MessagingConfiguration mCfg, GangliaConfiguration gCfg) {
+    this.mCfg = mCfg;
+    this.gCfg = gCfg;
+    this.poolSize = poolSize;
+    this.queue = queue;
+    registry = new MetricRegistry(queue);
+    registry.registerAll(new MemoryUsageGaugeSet());
+    registry.register(Metrics.OPEN_FILES, new FileDescriptorRatioGauge());
+  }
+
+  protected String regName(String name) {
+    return registry.getName() + "." + name;
+  }
+
+  /**
+   * Gets the clb DataSource instance from the existing guice injector.
+   * Make sure the injector has the ChecklistBankServiceMyBatisModule bound.
+   */
+  protected void initDbPool(Injector inj) {
+    hds = (HikariDataSource) inj.getInstance(InternalChecklistBankServiceMyBatisModule.DATASOURCE_KEY);
+  }
+
+  @Override
+  protected void startUp() throws Exception {
+    gCfg.start(registry);
+
+    publisher = new DefaultMessagePublisher(mCfg.getConnectionParameters());
+
+    // dataset messages are slow, long running processes. Only prefetch one message
+    listener = new MessageListener(mCfg.getConnectionParameters(), 1);
+    startUpBeforeListening();
+    listener.listen(queue, poolSize, this);
+  }
+
+  /**
+   * Hook to bind startup code to that gets executed before the listener actually starts listening to messages!
+   */
+  protected void startUpBeforeListening() throws Exception {
+    // dont do anything by default
+  }
+
+  @Override
+  protected void shutDown() throws Exception {
+    if (hds != null) {
+      hds.close();
     }
-
-    protected String regName(String name) {
-        return registry.getName() + "." + name;
+    if (listener != null) {
+      listener.close();
     }
-
-    /**
-     * Gets the clb DataSource instance from the existing guice injector.
-     * Make sure the injector has the ChecklistBankServiceMyBatisModule bound.
-     */
-    protected void initDbPool(Injector inj) {
-        hds = (HikariDataSource) inj.getInstance(InternalChecklistBankServiceMyBatisModule.DATASOURCE_KEY);
+    if (publisher != null) {
+      publisher.close();
     }
+  }
 
-    @Override
-    protected void startUp() throws Exception {
-        gCfg.start(registry);
-
-        publisher = new DefaultMessagePublisher(mCfg.getConnectionParameters());
-
-        // dataset messages are slow, long running processes. Only prefetch one message
-        listener = new MessageListener(mCfg.getConnectionParameters(), 1);
-        listener.listen(queue, poolSize, this);
+  protected void send(Message msg) throws IOException {
+    try {
+      LOG.info("Sending {}", msg.getClass().getSimpleName());
+      publisher.send(msg);
+    } catch (IOException e) {
+      LOG.error("Could not send {}", msg.getClass().getSimpleName(), e);
+      throw e;
     }
-
-    @Override
-    protected void shutDown() throws Exception {
-        if (hds != null) {
-            hds.close();
-        }
-        if (listener != null) {
-            listener.close();
-        }
-        if (publisher != null) {
-            publisher.close();
-        }
-    }
-
-    protected void send(Message msg) throws IOException {
-        try {
-            LOG.info("Sending {}", msg.getClass().getSimpleName());
-            publisher.send(msg);
-        } catch (IOException e) {
-            LOG.error("Could not send {}", msg.getClass().getSimpleName(), e);
-            throw e;
-        }
-    }
+  }
 
 }
