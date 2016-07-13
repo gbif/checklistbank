b2726baffbd865e62fbeb926ce8d18cf1bd75de4
Markus Döring <m.doering@mac.com>
2016-07-13 18:07:45 +0200

    Use latest gbif commons for best ascii folding

--- checklistbank-nub/src/test/java/org/gbif/nub/lookup/similarity/ScientificNameSimilarityTest.java
+++ checklistbank-nub/src/test/java/org/gbif/nub/lookup/similarity/ScientificNameSimilarityTest.java
@@ -21,14 +21,14 @@ public class ScientificNameSimilarityTest {
 
     assertEquals(100d, sns.getSimilarity("abcdefg", "abcdefg"), 0.01d);
     assertEquals(80d, sns.getSimilarity("abcdefg", "amcdefg"), 0.01d);
-//    assertEquals(80d, sns.getSimilarity("abcdefg", "acdefg"), 0.01d);
+    assertEquals(80d, sns.getSimilarity("abcdefg", "abcdeg"), 0.01d);
 
     assertEquals(0d, sns.getSimilarity("abcdefg", "zyxvwu"), 0.01d);
     assertEquals(0d, sns.getSimilarity("abcdefg", "amncdefg"), 0.01d);
     assertEquals(0d, sns.getSimilarity("abcdefg", "adefg"), 0.01d);
-
     assertEquals(100d, sns.getSimilarity("abcdefg", "aabbccddeeffgg"), 0.01d);
-    assertEquals(100d, sns.getSimilarity("äöüæøåœđł", "aeoeueaeoeaaoedl"), 0.01d);
+    assertEquals(100d, sns.getSimilarity("äöüæøåœđł", "aouaeoaoedl"), 0.01d);
+
     assertEquals(100d, sns.getSimilarity("xAus", "Aus"), 0.01d);
     assertEquals(100d, sns.getSimilarity("x Aus", "Aus"), 0.01d);
     assertEquals(100d, sns.getSimilarity("×Aus", "Aus"), 0.01d);
@@ -45,7 +45,7 @@ public class ScientificNameSimilarityTest {
 
     assertEquals(95d, sns.getSimilarity("Linaria pedunculata", "Linaria pedinculata"), 0.01d);
     assertEquals(90d, sns.getSimilarity("Linaria pedunculata", "Lunaria pedunculata"), 0.01d);
-    assertEquals(95d, sns.getSimilarity("Linaria pedunculata", "Linariya pedonculata"), 0.01d);
+    assertEquals(85d, sns.getSimilarity("Linaria pedunculata", "Linariya pedonculata"), 0.01d);
     assertEquals(93.33d, sns.getSimilarity("Linaria pedunculata vulgaris", "Lunaria pedunculata vulgaris"), 0.01d);
     assertEquals(5d, sns.getSimilarity("Linaria pedunculata vulgaris", "Linaria pedunculata vandalis"), 0.01d);
     assertEquals(5d, sns.getSimilarity("Oreina elegans", "Orfelia elegans"), 0.01d);
