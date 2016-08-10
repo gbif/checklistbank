2edf39f938abc256e857eeac214e15800fc16cc5
Markus Döring <m.doering@mac.com>
2016-08-10 17:39:19 +0200

    Fix solr testing resources

--- checklistbank-solr/src/test/java/org/gbif/checklistbank/index/SolrIndexingTestModule.java
+++ checklistbank-solr/src/test/java/org/gbif/checklistbank/index/SolrIndexingTestModule.java
@@ -6,21 +6,21 @@ package org.gbif.checklistbank.index;
 import org.gbif.checklistbank.index.guice.SolrIndexingModulePrivate;
 import org.gbif.checklistbank.service.mybatis.guice.ChecklistBankServiceMyBatisModule;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 
-import com.google.common.base.Throwables;
 import com.google.common.io.Resources;
 import com.google.inject.AbstractModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Guice module that initializes the required classes and dependencies for the CLB indexer
  * using a mocked dataset service.
  */
 public class SolrIndexingTestModule extends AbstractModule {
+  private static final Logger LOG = LoggerFactory.getLogger(SolrIndexingTestModule.class);
 
   private final Properties properties;
 
@@ -44,10 +44,10 @@ public class SolrIndexingTestModule extends AbstractModule {
   private static void removeUnUsedSolrCfg(){
     try {
       Files.delete(Paths.get(Resources.getResource("solr/checklistbank/conf/hdfs_directory_factory.xml").toURI()));
-    } catch(IllegalArgumentException e){
+      LOG.info("Removed hdfs_directory_factory.xml from solr resources.");
+    } catch(Exception e){
       // already removed
-    } catch(URISyntaxException | IOException ex){
-      Throwables.propagate(ex);
+      LOG.warn("Failed to remove hdfs_directory_factory.xml from solr resources. Probably has been removed before", e.getMessage());
     }
   }
 }
