fb60343aedfc2cbda22bfb98daeb3bf148d539d4
Markus Döring <m.doering@mac.com>
2017-01-05 13:58:24 +0100

    Allow NameUsageMatchWsClientTest to work with broken nub-ws

--- checklistbank-ws-client/src/test/java/org/gbif/checklistbank/ws/client/NameUsageMatchWsClientTest.java
+++ checklistbank-ws-client/src/test/java/org/gbif/checklistbank/ws/client/NameUsageMatchWsClientTest.java
@@ -1,7 +1,8 @@
 package org.gbif.checklistbank.ws.client;
 
+import javax.ws.rs.core.UriBuilderException;
+
 import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientHandlerException;
 import com.sun.jersey.api.client.WebResource;
 import org.junit.Test;
 
@@ -11,20 +12,27 @@ public class NameUsageMatchWsClientTest {
 
   /**
    * Test http://dev.gbif.org/issues/browse/POR-2688
-   * We expect a client handler exception cause we did not setup the entire jersey stack properly.
+   * We expect a client handler or uniform interface exception cause we did not setup the entire jersey stack properly.
    * We only want to make sure there is no UriBuilderException thrown
    */
-  @Test(expected = ClientHandlerException.class)
+  @Test
   public void testCurlyBrackets() {
-    WebResource api = Client.create().resource("http://api.gbif.org/v1/species/match");
-    WebResource  q = api.queryParam("name", "Nereis southerni %7B%7Bnowrap Abdel-Moez & Humphries, 1955");
-    assertEquals(
-      "http://api.gbif.org/v1/species/match?name=Nereis+southerni+%7B%7Bnowrap+Abdel-Moez+%26+Humphries,+1955",
-      q.toString());
+    try {
+      WebResource api = Client.create().resource("http://api.gbif.org/v1/species/match");
+      WebResource  q = api.queryParam("name", "Nereis southerni %7B%7Bnowrap Abdel-Moez & Humphries, 1955");
+      assertEquals(
+        "http://api.gbif.org/v1/species/match?name=Nereis+southerni+%7B%7Bnowrap+Abdel-Moez+%26+Humphries,+1955",
+        q.toString());
 
-    NameUsageMatchWsClient client = new NameUsageMatchWsClient(api);
+      NameUsageMatchWsClient client = new NameUsageMatchWsClient(api);
 
-    client.match("Nereis southerni {{nowrap Abdel-Moez & Humphries, 1955", null, null, false, false);
+      client.match("Nereis southerni {{nowrap Abdel-Moez & Humphries, 1955", null, null, false, false);
+    } catch (RuntimeException e) {
+      if (e instanceof UriBuilderException) {
+        throw e;
+      }
+      System.out.println(e.toString());
+    }
   }
 
 }
