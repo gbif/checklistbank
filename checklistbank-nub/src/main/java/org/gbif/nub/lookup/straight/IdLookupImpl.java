99d6ae02486c121aeba385c40ad104506a2fdfd1
Markus Döring <m.doering@mac.com>
2017-01-19 13:50:11 +0100

    Fix sql typo

--- checklistbank-nub/src/main/java/org/gbif/nub/lookup/straight/IdLookupImpl.java
+++ checklistbank-nub/src/main/java/org/gbif/nub/lookup/straight/IdLookupImpl.java
@@ -118,8 +118,8 @@ public class IdLookupImpl implements IdLookup {
         cm.copyOut("COPY ("
             + "SELECT u.id, u.parent_fk, u.pp_synonym_fk, coalesce(NULLIF(trim(n.canonical_name), ''), n.scientific_name), n.authorship, n.year, u.rank, u.kingdom_fk, deleted is not null"
             + " FROM name_usage u join name n ON name_fk=n.id"
-            + " WHERE dataset_key = '" + Constants.NUB_DATASET_KEY + "'" + delClause + " AND pp_synonym_fk is not null)"
-            + " ORDER BY pp_synonym_fk"
+            + " WHERE dataset_key = '" + Constants.NUB_DATASET_KEY + "'" + delClause + " AND pp_synonym_fk is not null"
+            + " ORDER BY pp_synonym_fk)"
             + " TO STDOUT WITH NULL ''", writer);
         LOG.info("Added {} pro parte usages into id lookup", usages.size()-uCount);
       }
