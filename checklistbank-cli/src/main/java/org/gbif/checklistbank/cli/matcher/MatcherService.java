d220909e37bedb0eb14552d1e9f1cb3ba9453560
mdoering <mdoering@gbif.org>
2018-02-05 12:38:45 +0100

    Log matching summary when completed

--- checklistbank-cli/src/main/java/org/gbif/checklistbank/cli/matcher/MatcherService.java
+++ checklistbank-cli/src/main/java/org/gbif/checklistbank/cli/matcher/MatcherService.java
@@ -1,8 +1,12 @@
 package org.gbif.checklistbank.cli.matcher;
 
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import com.google.inject.Key;
 import org.gbif.checklistbank.cli.common.RabbitDatasetService;
 import org.gbif.checklistbank.index.guice.RealTimeModule;
 import org.gbif.checklistbank.index.guice.Solr;
+import org.gbif.checklistbank.nub.lookup.DatasetMatchSummary;
 import org.gbif.checklistbank.nub.lookup.NubMatchService;
 import org.gbif.checklistbank.service.DatasetImportService;
 import org.gbif.checklistbank.service.mybatis.guice.ChecklistBankServiceMyBatisModule;
@@ -12,10 +16,6 @@ import org.gbif.common.messaging.api.messages.MatchDatasetMessage;
 import org.gbif.nub.lookup.straight.DatasetMatchFailed;
 import org.gbif.nub.lookup.straight.IdLookup;
 import org.gbif.nub.lookup.straight.IdLookupImpl;
-
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.Timer;
-import com.google.inject.Key;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -62,8 +62,8 @@ public class MatcherService extends RabbitDatasetService<MatchDatasetMessage> {
     final Timer.Context context = timer.time();
     try {
       LOG.info("Start matching dataset {}", msg.getDatasetUuid());
-      matcher.matchDataset(msg.getDatasetUuid());
-      LOG.info("Dataset {} matched sucessfully", msg.getDatasetUuid());
+      DatasetMatchSummary summary = matcher.matchDataset(msg.getDatasetUuid());
+      LOG.info("Dataset {} matched sucessfully: {}", msg.getDatasetUuid(), summary);
       // now also request new metrics from the analysis step
       //ChecklistSyncedMessage triggers a new dataset analysis
       send(new ChecklistSyncedMessage(msg.getDatasetUuid(), new Date(), 0, 0));
