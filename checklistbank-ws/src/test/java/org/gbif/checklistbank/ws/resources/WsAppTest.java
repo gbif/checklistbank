6e67a65974b4d7b97d8db964f9d2cef565736ded
mdoering <mdoering@gbif.org>
2018-06-06 17:47:00 +0200

    Remove custom security manager after test finishes

--- checklistbank-ws/src/test/java/org/gbif/checklistbank/ws/resources/WsAppTest.java
+++ checklistbank-ws/src/test/java/org/gbif/checklistbank/ws/resources/WsAppTest.java
@@ -3,23 +3,41 @@ package org.gbif.checklistbank.ws.resources;
 import java.security.Permission;
 
 import org.gbif.ws.app.Application;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.fail;
 
 
 public class WsAppTest {
+  private SecurityManager sm;
+
+  @Before
+  public void init(){
+    sm = System.getSecurityManager();
+    System.setSecurityManager(new NoExitSecurityManager());
+  }
+
+  @After
+  public void shutdown(){
+    System.setSecurityManager(sm);
+  }
 
   /**
    * Test the startup of the webapp.
-   * We expect a SecurityException raised as the Application stops because of missing configs.
+   * We expect a ExitException raised as the Application stops because of missing configs.
    * But no NoSuchMethodError thrown by incompatible jackson we see with a wrong version introduced by solr
    */
-  @Test(expected = SecurityException.class)
+  @Test(expected = ExitException.class)
   public void testWsStartup() {
-    // throw exception if Application exits
-    System.setSecurityManager(new NoExitSecurityManager());
     Application.main(new String[]{});
+  }
+
+  public static class ExitException extends RuntimeException {
+    public ExitException(int status) {
+      super("Exit status " + status + " called");
+    }
   }
 
   private static class NoExitSecurityManager extends SecurityManager {
@@ -35,7 +53,7 @@ public class WsAppTest {
     @Override
     public void checkExit(int status) {
       super.checkExit(status);
-      throw new SecurityException("Exit status " + status + " called");
+      throw new ExitException(status);
     }
   }
 }
