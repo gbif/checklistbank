ebd6859d73ae588aa295fbc19ed3eb72c65b2f82
mdoering <mdoering@gbif.org>
2018-10-18 10:38:07 +0200

    Use latest apache tika and jackson2 fixing GH security alerts

--- checklistbank-nub/src/main/java/org/gbif/nub/lookup/fuzzy/NubMatchingServiceImpl.java
+++ checklistbank-nub/src/main/java/org/gbif/nub/lookup/fuzzy/NubMatchingServiceImpl.java
@@ -434,8 +434,6 @@ public class NubMatchingServiceImpl implements NameUsageMatchingService, NameUsa
     List<NameUsageMatch> matches = null;
     switch (mode) {
       case FUZZY:
-        matches = queryFuzzy(queryNameType, pn, canonicalName, rank, lc, verbose);
-        break;
       case STRICT:
         matches = queryStrict(queryNameType, pn, canonicalName, rank, lc, verbose);
         break;
