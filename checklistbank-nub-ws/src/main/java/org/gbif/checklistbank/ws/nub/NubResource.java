f6bbd104d7fc6547b2430d2acdc57842947ce337
Markus <mdoering@gbif.org>
2020-11-23 15:50:04 +0100

    Fix #110

--- checklistbank-nub-ws/src/main/java/org/gbif/checklistbank/ws/nub/NubResource.java
+++ checklistbank-nub-ws/src/main/java/org/gbif/checklistbank/ws/nub/NubResource.java
@@ -1,6 +1,7 @@
 package org.gbif.checklistbank.ws.nub;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import org.gbif.api.model.checklistbank.NameUsageMatch;
@@ -8,6 +9,7 @@ import org.gbif.api.model.common.LinneanClassification;
 import org.gbif.api.util.VocabularyUtils;
 import org.gbif.api.v2.NameUsageMatch2;
 import org.gbif.api.vocabulary.Rank;
+import org.gbif.api.model.checklistbank.ParsedName;
 import org.gbif.nub.lookup.NameUsageMatchingService2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,13 +20,20 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Path("/species")
 @Singleton
 public class NubResource {
 
   private static final Logger LOG = LoggerFactory.getLogger(NubResource.class);
-
+  private static final List<Rank> REVERSED_DWC_RANKS = new ArrayList<>(Rank.DWC_RANKS);
+  static {
+    Collections.reverse(REVERSED_DWC_RANKS);
+  }
   private final NameUsageMatchingService2 matchingService;
 
   @Inject
@@ -35,21 +44,96 @@ public class NubResource {
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("match")
-  public NameUsageMatch match(@QueryParam("name") String scientificName, @QueryParam("rank") String rank,
+  public NameUsageMatch match(@QueryParam("name") String scientificName2, @QueryParam("scientificName") String scientificName,
+                              @QueryParam("authorship") String authorship2, @QueryParam("scientificNameAuthorship") String authorship,
+                              @QueryParam("rank") String rank2, @QueryParam("taxonRank") String rank,
+                              @QueryParam("specificEpithet") String specificEpithet,
+                              @QueryParam("infraspecificEpithet") String infraspecificEpithet,
                               @Context LinneanClassification classification,
                               @QueryParam("strict") Boolean strict, @QueryParam("verbose") Boolean verbose) {
-    Rank r = parse(Rank.class, rank);
-    return matchingService.match(scientificName, r, classification, bool(strict), bool(verbose));
+    Rank r = parse(Rank.class, first(rank, rank2));
+    return match(first(scientificName, scientificName2),
+                 first(authorship, authorship2),
+                 specificEpithet,
+                 infraspecificEpithet,
+                 r, classification, bool(strict), bool(verbose)
+    );
   }
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("match2")
-  public NameUsageMatch2 match2(@QueryParam("name") String scientificName, @QueryParam("rank") String rank,
+  public NameUsageMatch2 match2(@QueryParam("name") String scientificName2, @QueryParam("scientificName") String scientificName,
+                                @QueryParam("authorship") String authorship2, @QueryParam("scientificNameAuthorship") String authorship,
+                                @QueryParam("rank") String rank2, @QueryParam("taxonRank") String rank,
+                                @QueryParam("specificEpithet") String specificEpithet,
+                                @QueryParam("infraspecificEpithet") String infraspecificEpithet,
                                 @Context LinneanClassification classification,
                                 @QueryParam("strict") Boolean strict, @QueryParam("verbose") Boolean verbose) {
-    Rank r = parse(Rank.class, rank);
-    return matchingService.v2(matchingService.match(scientificName, r, classification, bool(strict), bool(verbose)));
+    Rank r = parse(Rank.class, first(rank, rank2));
+    return matchingService.v2(match(first(scientificName, scientificName2),
+        first(authorship, authorship2),
+        specificEpithet,
+        infraspecificEpithet,
+        r, classification, bool(strict), bool(verbose)
+    ));
+  }
+  
+  private NameUsageMatch match(String scientificName, String authorship, String specificEpithet, String infraSpecificEpithet,
+                       Rank rank, LinneanClassification classification, Boolean strict, Boolean verbose) {
+    if (!Strings.isNullOrEmpty(scientificName)) {
+      // maybe add authorship, ignore rest
+      scientificName = appendAuthorship(scientificName, authorship);
+
+    } else if (classification != null) {
+      Rank clRank = lowestRank(classification);
+      if (clRank != null) {
+        // atomized binomial?
+        if (!Strings.isNullOrEmpty(classification.getGenus()) && !Strings.isNullOrEmpty(specificEpithet)) {
+          ParsedName pn = new ParsedName();
+          pn.setGenusOrAbove(classification.getGenus());
+          pn.setInfraGeneric(classification.getSubgenus());
+          pn.setSpecificEpithet(specificEpithet);
+          pn.setInfraSpecificEpithet(infraSpecificEpithet);
+          pn.setRank(rank);
+          pn.setAuthorship(authorship);
+          scientificName = pn.canonicalName();
+        } else {
+          rank = clRank;
+          scientificName = appendAuthorship(classification.getHigherRank(clRank), authorship);
+        }
+      }
+    }
+    return matchingService.match(scientificName, rank, classification, bool(strict), bool(verbose));
+  }
+
+  static String appendAuthorship(String scientificName, String authorship){
+    if (!Strings.isNullOrEmpty(scientificName)
+        && !Strings.isNullOrEmpty(authorship)
+        && !scientificName.toLowerCase().contains(authorship.toLowerCase())) {
+      return scientificName + " " + authorship;
+    }
+    return scientificName;
+  }
+
+  static Rank lowestRank(LinneanClassification cl){
+    for (Rank r : REVERSED_DWC_RANKS) {
+      if (!Strings.isNullOrEmpty(cl.getHigherRank(r))) {
+        return r;
+      }
+    }
+    return null;
+  }
+
+  static String first(String... values){
+    if (values != null) {
+      for (String val : values) {
+        if (!Strings.isNullOrEmpty(val)) {
+          return val;
+        }
+      }
+    }
+    return null;
   }
 
   private <T extends Enum> T parse(Class<T> clazz, String value) throws IllegalArgumentException {
