f57c839c2f9a67324e1d34e823e77793e8e74987
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2022-03-11 13:57:14 +0100

    copied AvroExporter to ES module

---
+++ checklistbank-elasticsearch/src/main/java/org/gbif/checklistbank/exporter/NameUsageIndexingJob.java
@@ -0,0 +1,152 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gbif.checklistbank.exporter;
+
+import org.gbif.api.model.checklistbank.*;
+import org.gbif.checklistbank.index.NameUsageDocConverter;
+import org.gbif.checklistbank.model.UsageExtensions;
+import org.gbif.checklistbank.service.UsageService;
+import org.gbif.checklistbank.service.mybatis.service.DescriptionServiceMyBatis;
+import org.gbif.checklistbank.service.mybatis.service.DistributionServiceMyBatis;
+import org.gbif.checklistbank.service.mybatis.service.SpeciesProfileServiceMyBatis;
+import org.gbif.checklistbank.service.mybatis.service.VernacularNameServiceMyBatis;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.time.StopWatch;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.common.SolrInputDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Executable job that creates a list of {@link SolrInputDocument} using a list of {@link NameUsage} objects.
+ */
+public class NameUsageIndexingJob implements Callable<Integer> {
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
+  private static final int batchSize = 100;
+
+  /**
+   * SolrServer instance.
+   */
+  private final SolrClient solrClient;
+
+  /**
+   * Minimum usage key, inclusive, to process.
+   */
+  private final int startKey;
+
+  /**
+   * Maximum usage key, inclusive, to process.
+   */
+  private final int endKey;
+
+  /**
+   * Service layer.
+   */
+  private final UsageService nameUsageService;
+  private final VernacularNameServiceMyBatis vernacularNameService;
+  private final DescriptionServiceMyBatis descriptionService;
+  private final DistributionServiceMyBatis distributionService;
+  private final SpeciesProfileServiceMyBatis speciesProfileService;
+
+  private StopWatch stopWatch = new StopWatch();
+
+  /**
+   * {@link NameUsage}/{@link SolrInputDocument} converter.
+   */
+  private final NameUsageDocConverter solrDocumentConverter;
+
+  /**
+   * Default constructor.
+   */
+  public NameUsageIndexingJob(final SolrClient solrClient, final UsageService nameUsageService, final int startKey,
+    final int endKey, final NameUsageDocConverter solrDocumentConverter,
+    final VernacularNameServiceMyBatis vernacularNameService, final DescriptionServiceMyBatis descriptionService,
+    final DistributionServiceMyBatis distributionService, final SpeciesProfileServiceMyBatis speciesProfileService) {
+    this.nameUsageService = nameUsageService;
+    this.vernacularNameService = vernacularNameService;
+    this.descriptionService = descriptionService;
+    this.distributionService = distributionService;
+    this.speciesProfileService = speciesProfileService;
+    this.startKey = startKey;
+    this.endKey = endKey;
+    this.solrDocumentConverter = solrDocumentConverter;
+    this.solrClient = solrClient;
+  }
+
+  /**
+   * Iterates over the assigned {@link NameUsage} objects to insert the corresponding {@link SolrInputDocument}
+   * objects.
+   *
+   * @return the total number of documents added by this Thread.
+   */
+  @Override
+  public Integer call() throws Exception {
+    // Timing information initialization
+    stopWatch.start();
+    log.info("Adding usages from id {} to {}", startKey, endKey);
+    int docCount = 0;
+    // Get all usages
+    List<NameUsage> usages = nameUsageService.listRange(startKey, endKey);
+    // get all component maps into memory first
+    Map<Integer, List<VernacularName>> vernacularNameMap = vernacularNameService.listRange(startKey, endKey);
+
+    Map<Integer, List<Description>> descriptionMap = descriptionService.listRange(startKey, endKey);
+
+    Map<Integer, List<Distribution>> distributionMap = distributionService.listRange(startKey, endKey);
+
+    Map<Integer, List<SpeciesProfile>> speciesProfileMap = speciesProfileService.listRange(startKey, endKey);
+
+    // now we're ready to build the solr indices quicky!
+    for (Iterable<NameUsage> batch : Iterables.partition(usages, batchSize)) {
+      final List<SolrInputDocument> docs = Lists.newArrayList();
+      try {
+        for (NameUsage usage : batch) {
+          if (usage==null) {
+            log.warn("Unexpected null usage found in range {}-{}, docCount={}", startKey, endKey, docCount);
+            continue;
+          }
+          UsageExtensions ext = new UsageExtensions();
+          ext.speciesProfiles = speciesProfileMap.get(usage.getKey());
+          ext.vernacularNames = vernacularNameMap.get(usage.getKey());
+          ext.descriptions = descriptionMap.get(usage.getKey());
+          ext.distributions = distributionMap.get(usage.getKey());
+
+          List<Integer> parents = nameUsageService.listParents(usage.getKey());
+
+          docs.add(solrDocumentConverter.toDoc(usage, parents, ext));
+          docCount++;
+        }
+        solrClient.add(docs);
+        NameUsageBatchProcessor.counter.addAndGet(docs.size());
+
+      } catch (Exception e) {
+        log.error("Error indexing document for usage batch", e);
+      }
+    }
+
+    // job finished notice
+    stopWatch.stop();
+    log.info("Finished indexing of usages in range {}-{}. Total time: {}", startKey, endKey, stopWatch.toString());
+
+    return docCount;
+  }
+
+}
---
+++ checklistbank-elasticsearch/src/main/java/org/gbif/checklistbank/exporter/ThreadPoolRunner.java
@@ -0,0 +1,160 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gbif.checklistbank.exporter;
+
+import java.util.List;
+import java.util.concurrent.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class was implemented to be executed only one time, and probably be executed by a job scheduler (CRON for example).
+ * The behavior of the pool of threads can be customized using the properties file, using this file
+ * * is possible define the values for: poolSize, maxPoolSize and keepAliveTime; and additionally for the
+ * e-mail configuration (an email will be sent at the end of the process):
+ *  smtp.server,email.to,email.from,email.subject,email.body,email.cc
+ * The thread pool is implemented using an {@link ExecutorCompletionService} and a {@link ThreadPoolExecutor} because
+ * some results can be return for each job: errors during the process or any other information.
+ */
+public abstract class ThreadPoolRunner<T> {
+
+
+  private static class DefaultRejectedExecutionHandler implements RejectedExecutionHandler {
+
+    @Override
+    public void rejectedExecution(Runnable job, ThreadPoolExecutor executor) {
+      ThreadPoolRunner.LOG.debug("Job has been rejected by the executor, putting on queue");
+      try {
+        executor.getQueue().put(job);
+      } catch (InterruptedException e1) {
+        LOG.error("Work discarded, thread was interrupted while waiting for space to schedule: {}", job);
+      }
+    }
+  }
+
+
+  /**
+   * the number of threads to keep in the pool, even if they are idle.
+   */
+  protected int poolSize = 0;
+
+  /**
+   * Executes each submitted task (agent synchronization) using one of possibly several pooled threads
+   */
+  private ThreadPoolExecutor threadPool = null;
+
+  /**
+   * Decouples the production of new asynchronous tasks from the consumption of the results of completed tasks
+   */
+  private CompletionService<T> completionService;
+
+  /**
+   * Queue of elements that will be executed by the {@link ThreadPoolExecutor}
+   */
+  private final BlockingQueue<Runnable> queue = new SynchronousQueue<Runnable>();
+
+  /**
+   * Logger for the {@link ThreadPoolRunner} class
+   */
+  private static final Logger LOG = LoggerFactory.getLogger(ThreadPoolRunner.class);
+
+  /**
+   * @param poolSize the number of threads to keep in the pool, even if they are idle.
+   */
+  public ThreadPoolRunner(int poolSize) {
+    this.poolSize = poolSize;
+  }
+
+  protected abstract Callable<T> newJob();
+
+  /**
+   * Initialize the {@link ThreadPoolExecutor} and {@link ExecutorCompletionService}
+   */
+  private void initThreadPool() {
+    threadPool = new ThreadPoolExecutor(
+      poolSize, poolSize, 0, TimeUnit.SECONDS, queue, new DefaultRejectedExecutionHandler()
+    );
+    completionService = new ExecutorCompletionService<T>(threadPool);
+  }
+
+
+  /**
+   * This method runs the synchronization for all the agent of the same type,
+   * creates a thread that will synchronized # of agents (defined by the agentsPerThread field)
+   */
+  public int run() {
+    // localTasksCount contains the number of threads created, this count will be used for shutting down the service
+
+    initThreadPool();
+
+    int localTasksCount = 0;
+    try {
+      // gets the agents with service type: this.serviceTypeCode
+      Callable<T> job = newJob();
+      while (job != null) {
+        try {
+          LOG.debug("Submitting job {}, tasks count={}", job, localTasksCount);
+          completionService.submit(job);
+          localTasksCount += 1;
+        } catch (Exception e) {
+          LOG.error("Error when submiting job: {}", localTasksCount);
+        }
+        job = newJob();
+      }
+      LOG.info("All {} jobs submitted succesfully!", localTasksCount);
+      shutdownService(localTasksCount);
+    } catch (Exception e) {
+      LOG.error("Error when submiting jobs: {}", e);
+    }
+
+    return localTasksCount;
+  }
+
+  /**
+   * Performs the shutdown activities: waits each thread to complete the process and collects the results.
+   */
+  protected void shutdownService(int tasksCount) {
+    try {
+      LOG.info("Initiating shutting down of the service");
+      while (tasksCount > 0) {
+        // waits for each task to return the results
+        Future<T> result = completionService.take();
+        tasksCount--;
+        try {
+          T taskResponse = result.get();
+          LOG.info("Job completed, {} still running", tasksCount);
+          taskResponseHook(taskResponse);
+
+        } catch (ExecutionException e) {
+          LOG.error("Error waiting a job to return results. {} still running jobs", tasksCount, e);
+        }
+        LOG.debug("Waiting for {} jobs to shutdown.", tasksCount);
+      }
+      List<Runnable> failedTasks = threadPool.shutdownNow();
+      LOG.debug("{} Tasks failed or forcibly removed from the thread pool", failedTasks.size());
+    } catch (Exception ignored) {
+      LOG.error("Thread pool prematurely terminated", ignored);
+    }
+    LOG.info("Shutdown complete");
+  }
+
+  /**
+   * Override this method if the main runner needs to consume the individual job results.
+   * @param taskResponse the finished job result
+   */
+  protected void taskResponseHook (T taskResponse) {
+
+  }
+}
